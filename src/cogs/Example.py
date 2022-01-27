e318d37184859a9a597c1afcc579554e65bb781d
censor <86632275+cens6r@users.noreply.github.com>
2022-01-27 10:22:17 -0800

    Create Example.py

---
+++ src/cogs/Example.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import commands
+
+class Example(commands.Cog):
+
+    def __init__(self , Client):
+        self.Client = Client
+
+    @commands.command()
+    async def helloworld(self , ctx):
+      ctx.send('Hello World!')
+      
+def setup(Client):
+    Client.add_cog(Example(Client))
