e3f4fba8dc9e1c3c0ff994eee7d4b50476d91c0c
censor <86632275+cens6r@users.noreply.github.com>
2022-02-21 20:25:37 -0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,10 +3,10 @@ import discord, os, json
 from discord.ext import commands
 from dotenv import load_dotenv
 
-dotenv_path = os.path.join(os.path.dirname(__file__), 'configurations/.env')
+dotenv_path = os.path.join(os.path.dirname(__file__), 'configurations\\.env')
 load_dotenv(dotenv_path)
 
-with open(os.path.join(os.path.dirname(__file__), 'configurations/config.json'), 'r') as config:
+with open(os.path.join(os.path.dirname(__file__), 'configurations\\config.json'), 'r') as config:
   get = json.load(config)
   
 prefix = get['prefix']
@@ -26,7 +26,7 @@ async def load(ctx , extension):
 async def unload(ctx , extension):
     Client.unload_extension(f"cogs.{extension}")
 
-for filename in os.listdir(os.path.dirname(os.path.abspath(__file__)) + '/cogs'):
+for filename in os.listdir(os.path.dirname(os.path.abspath(__file__)) + '\\cogs'):
     if filename.endswith(".py"):
         Client.load_extension(f"cogs.{filename[:-3]}")
 
