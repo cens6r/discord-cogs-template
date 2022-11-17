6bc0309c3bfe14069a1f8a52ecd431eb31e4debc
censor <86632275+cens6r@users.noreply.github.com>
2022-11-17 12:37:15 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -15,17 +15,15 @@ intents = discord.Intents().all()
 Client = commands.Bot(command_prefix=prefix, case_insensitive=True, intents=intents, help_command=None)
 
 @Client.command()
-async def load(ctx , extension):
+async def load(ctx, extension):
   try:
     Client.unload_extension(f"cogs.{extension}")
   except Exception:
     pass
   Client.load_extension(f"cogs.{extension}")
-
-@Client.command()
-async def unload(ctx , extension):
-    Client.unload_extension(f"cogs.{extension}")
-
+  e = discord.Embed(title=f"{extension} has been reloaded.", color=discord.Color.dark_green())
+  await ctx.reply(embed=e)
+  
 for filename in os.listdir(os.path.dirname(os.path.abspath(__file__)) + '\\cogs'):
     if filename.endswith(".py"):
         Client.load_extension(f"cogs.{filename[:-3]}")
